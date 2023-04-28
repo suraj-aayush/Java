b763dd3d77346690b7f204760b9b9a764468e006
Aayush Kumar <105626112+suraj-aayush@users.noreply.github.com>
2023-04-29 00:59:53 +0530

    Add files via upload

---
+++ Packages/file1.java
@@ -0,0 +1,12 @@
+package mypack.newfile1;
+public class file1 {
+  public static void func()
+    {
+        System.out.println("function uses ");
+    }
+    public static void main(String[] args) {
+        System.out.println("file 1 here ");
+
+
+    }
+}
