0d1438f57e3d3cbaa897826569ac79f43cc8330e
Aayush Kumar <105626112+suraj-aayush@users.noreply.github.com>
2023-05-01 01:37:23 +0530

    Add files via upload

---
+++ Classes and Objects/shapeMatch.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class shapeMatch{
+    class shapes{
+
+        
+    }
+    class rectangle extends shapes{
+        int area;
+        String colour;
+        int l,b;
+
+
+    }
+    class square extends shapes{
+        int l;
+        int area;
+        String colour;
+        
+    }
+public static void main(String[] args) 
+    {
+        
+    }
+}
