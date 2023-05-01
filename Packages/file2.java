ae0c195453ed6190774ad1c4971274e79e735a48
Aayush Kumar <105626112+suraj-aayush@users.noreply.github.com>
2023-05-02 00:33:42 +0530

    Add files via upload

---
+++ Packages/file2.java
@@ -0,0 +1,10 @@
+package mypack.newfile2;
+public class file2 {
+    public static void main(String[] args) {
+        System.out.println("file 2 here ");
+    }
+}
+
+//  compile code
+
+//  javac -d . *.java;
