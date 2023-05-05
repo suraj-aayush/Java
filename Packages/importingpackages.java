da6d35c062ef7cb22fa4ace928edb08860151620
Aayush Kumar <105626112+suraj-aayush@users.noreply.github.com>
2023-05-05 15:50:06 +0530

    Add files via upload

---
+++ Packages/importingpackages.java
@@ -0,0 +1,8 @@
+import mypack.file1.func;
+public class importingpackages {
+    public static void main(String[] args) {
+        System.out.println("import successfully ");
+      file1 sc = new file1();
+        sc.func();
+    }
+}
